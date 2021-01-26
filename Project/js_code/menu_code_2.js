202306940de209f365a5722d592b4f6ea79e2da7
AbdulRahimShaikh1999 <abdulshaikhr99@gmail.com>
2021-01-25 20:25:41 -0500

    project for interview assessment

---
+++ Project/js_code/menu_code_2.js
@@ -0,0 +1,5 @@
+function menuOnClick2() {
+    document.getElementById("menu-bar-2").classList.toggle("change-2");
+    document.getElementById("nav-2").classList.toggle("change-2");
+    document.getElementById("menu-bg-2").classList.toggle("change-bg-2");
+  }
---
+++ Project/js_code/menu_code_3.js
@@ -0,0 +1,5 @@
+function menuOnClick3() {
+    document.getElementById("menu-bar-3").classList.toggle("change-3");
+    document.getElementById("nav-3").classList.toggle("change-3");
+    document.getElementById("menu-bg-3").classList.toggle("change-bg-3");
+  }
