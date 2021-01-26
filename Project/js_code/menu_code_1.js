2fce8c13f1d3d7f9d53adfc375228f62c37bf119
AbdulRahimShaikh1999 <69612923+AbdulRahimShaikh1999@users.noreply.github.com>
2021-01-25 20:22:35 -0500

    Update menu_code_1.js

--- Project/js_code/menu_code_1.js
+++ Project/js_code/menu_code_1.js
@@ -1,5 +1,5 @@
 function menuOnClick1() {
-    document.getElementById("menu-bar-1").classList.toggle("change-1"); // testing
+    document.getElementById("menu-bar-1").classList.toggle("change-1");
     document.getElementById("nav-1").classList.toggle("change-1");
     document.getElementById("menu-bg-1").classList.toggle("change-bg-1");
   }
